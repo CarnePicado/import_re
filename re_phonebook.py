a32a1d441513372d9df85a97a1240ef9d1db93ef
Ivan Aleinikov <cainepicado@gmail.com>
2023-04-01 16:15:20 +0300

    new change in re_phonebook

--- re_phonebook.py
+++ re_phonebook.py
@@ -1,17 +1,20 @@
+from pprint import pprint
+from collections import defaultdict
 import csv, os, re, sys
+
 
 def read_file(file_path, file_name):
   with open(os.path.join(file_path, file_name), encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
     return contacts_list
-
+  
 
 def format_number(contacts_list):
   number_pattern = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)' \
-  r'(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)(\d{2})' \
-  r'(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
-  repl = r'+7(\4)\8-\11-\14\15\17\18\19\20'
+                  r'(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)' \
+                  r'(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
+  repl = r'+7(\4)\8-\11-\14\15\17\18\20'
   new_contacts_list = []
   for contact in contacts_list:
     str_contact = ','.join(contact)
@@ -22,37 +25,27 @@ def format_number(contacts_list):
 
 
 def format_name(contacts_list):
-  name_pattern = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)(\s*)' \
-    r'(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
+  name_pattern = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)' \
+                r'(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
   repl = r'\1\3\10\4\6\9\7\8'
   new_contacts_list = []
   for contact in contacts_list:
     str_contact = ','.join(contact)
     format_contact = re.sub(name_pattern, repl, str_contact)
     lst_contact = format_contact.split(',')
-    new_contacts_list.append(lst_contact)
+    if lst_contact not in new_contacts_list:
+      new_contacts_list.append(lst_contact)
   return new_contacts_list
 
 
 def correct_text(contacts_list):
-  for i in contacts_list:
-    for j in contacts_list:
-      if i[0] == j[0] and i[1] == j[1] and i is not j:
-        if i[2] == '':
-          i[2] = j[2]
-        if i[3] == '':
-          i[3] = j[3]
-        if i[4] == '':
-          i[4] = j[4]
-        if i[5] == '':
-          i[5] = j[5]
-        if i[6] == '':
-          i[6] = j[6]
-  new_contacts_list = []
+  new_list = defaultdict(list)
   for contact in contacts_list:
-    if contact not in new_contacts_list:
-      new_contacts_list.append(contact)
-  return new_contacts_list
+    key = tuple(contact[:2])
+    for part in contact:
+      if part not in new_list[key]:
+        new_list[key].append(part)
+  return list(new_list.values())
 
 
 def write_file(contacts_list):
